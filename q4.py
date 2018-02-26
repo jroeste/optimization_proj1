bd586c9f09dfda704865504a7a46cb0ad2b31e53
Even Myklebust <even.myklebust@gmail.com>
2018-02-26 21:12:16 +0100

    fiksa q4 og starter q5

--- q4.py
+++ q4.py
@@ -29,22 +29,19 @@ def f_model_2(z_list,A,b):
             functionsum+=(max(1-np.dot(z_list[i][1:],np.matmul(A,z_list[i][1:]))-np.dot(b,z_list[i][1:]),0))**2
     return functionsum
 
-
-
-def construct_A_and_C(n,x):
+def construct_A_and_C(n,x): # Her har Even vært og endret ting 26.02
+    C=x[int(n*(n+1)/2):]
     A=np.zeros((n,n))
-    C=x[n*(n+1)/2:]
     index=0
-    counter=0
     for h in range(n):
         for j in range(n-h):
+            A[h][h + j] = x[index]
+            A[h + j][h] = x[index]
             index+=1
-            A[h][j + h] = x[index]
-            #A[i][j+i]=x[n*i+counter+j]  #should change to x[counter], where counter increases with 1.
-        counter -= h
-        for j in range(h):
-            A[h][j]=A[j][h]
-    return A,C
+    print("x\n", x)
+    print("A\n", A)
+    print("C contains elements in x from index", int(n*(n+1)/2), "\n", C)
+    return A, C
 
 def df_model_1(z_list,n,A,b,c): #if model 1: b=0 and c=c, if model 2: b=b and c=0
     counter=0
@@ -69,18 +66,22 @@ def df_model_1(z_list,n,A,b,c): #if model 1: b=0 and c=c, if model 2: b=b and c=
                     dfx[int(n*(n+1)/2)+j]+=-2*compute_r_i_1(z_list[i],A,c,i)*A[h][j]*(z_list[i][h+1]-c[h])   #legg til alpha
     return dfx
 
-if __name__ == "__main__":
-    n = 6  # dimensions
+if __name__ == "__main__":  # Her har Even vært og endret ting 26.02
+    # dimensions must be n = int(k*(k+1)/2) such that k is an integer
+    n = 6
+    dim = int(n*(n+1)/2) + n
     m = 3  # number of z points
-    x = np.ones(n)
-    z_list = np.zeros(m)
+    x = np.ones(dim)
+    z_list = np.zeros((m, n + 1))
     for i in range(m):
         z_list[i] = np.ones(n + 1) * i
         if i < int(m / 2):
             z_list[i][0] = -1
         else:
             z_list[i][0] = 1
-
-    # m=3 gir to -1 og en 1
+    print("n", n)
+    print("z_list\n",z_list)
+    print("dim of x\n", dim)
+    # m=3 gir to w = -1 og en w = 1
     A, c = construct_A_and_C(n, x)
-    f_model_1(z_list)
+    print("value of model 1:", f_model_1(z_list, A, c))
---
+++ q5.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import q4.py as q4
+
+# Three optimization algorithms
+
+def steepestDescent():
+    return 0
