67b77c319ed0de8b7016863bc6ecc8df19e4a029
Even Myklebust <even.myklebust@gmail.com>
2018-03-01 15:09:05 +0100

    BFGS og Steepest descent fungerer

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 ﻿# optimization_proj1
 Optimization Project 1
 
-26.02
-Endret i q4: construct A and C
+Alle algoritmer sender x imellom, A b c opprettes lokalt.
 
-Må prøve å opprette A og c færrest mulig ganger. Må optimere mhp x, men prøver å alltid sende A og c imellom funksjonene
+Optimeringsalgoritmer: 
+Beste metode for å bestemme steglengde: Conjugate Gradient
+Fletcher and Reeves -> Exact linesearch
+Quasi-Newton BFGS
+Evt Trust Region Methods in contrast to Line Search Methods
 
-Status: 
-
-Har opretta et utkast til Steepest Descent for å finne search direction og Backtracking Linesearch for å finne steplength. 
-
-Må sette meg inn i df_model_1 for å skjønne hva som må fikses.
-
-I linje 24 i q5 må det være et tall som lages og ikke en liste. Det er det første som må fikses.
+Logg:
+I linje 24 i q5 må det være et tall som lages og ikke en liste.
 
 jupyter token:
 http://127.0.0.1:8888/?token=505c7c24a4fa9f2ae6044de6760c3a7d386b7652d4fb03c7
--- q5.py
+++ q5.py
@@ -1,75 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 import q4 as q4
 
-# Two conditions to guarantee convergence:
-#   Armijo condition = sufficient decrease condition
-#   Curvature condition
-
-# f trenger z_list, A, c        df trenger z_list, n, A, b, c
+# Optimization Algorithms
 
 def backtrackingLinesearch(f, df, z_list, n, p, x): # Satisfies sufficient decrease, doesn't care about curvature condition
     alpha0 = 1
     rho = 0.5
-    c1 = 0.5
+    c1 = 0.4
     alpha = alpha0
-    A0, c0 = q4.construct_A_and_C(n, x)
-    f0 = f(z_list, A0, c0)
-    while True:
-        A, c = q4.construct_A_and_C(n, x + alpha*p)
-        b = 0
-        # Her oppretter vi matrise på nytt hver gang... Må vi det?
-        #if f(x + alpha*p) <= f(z_list, x) + c1*alpha*df(x)*p:
-        print("f", f(z_list, A, c))
-        print("f0 + c1 * alpha * df(z_list, n, A, b, c) * p", f0 + c1 * alpha * df(z_list, n, A, b, c) * p)
-        return alpha
-#        if f(z_list, A, c) <= f0 + c1 * alpha * df(z_list, n, A, b, c) * p:
-#            return alpha
-#        else:
-#            alpha = rho * alpha
+    f0 = f(z_list, n, x)
+    while True:  #in the worst case it takes a small step
+        if f(z_list, n, x + alpha*p) <= f0 + c1 * alpha * np.dot(df(z_list, n, x), p):
+            return alpha
+        else:
+            alpha = rho * alpha
 
-# kan evt lage en core linesearch method som motor inne i funksjoner steepest descent, newton etc for å finne p
+def armijoBacktracking(f, df, z_list, n, p, x): # Both conditions satisfied, using bisection
+    alpha0 = 1
+    alpha_max = np.inf
+    alpha_min = 0
+    c1 = 0.4
+    c2 = 0.8
+    alpha = alpha0
+    while True:
+        # Suff decr. Sjekker om vi har gått for langt
+        if f(z_list, n, x + alpha*p) >= f(z_list, n, x) + c1 * alpha * np.dot(df(z_list, n, x), p):
+            alpha_max = alpha
+            alpha = (alpha_min + alpha_max)/2
+        # Curvature. Sjekker om vi har gått for kort
+        elif np.dot(df(z_list, n, x + alpha*p), p) < c2 * np.dot(df(z_list, n, x), p):
+            alpha_min = alpha
+            if alpha_max == np.inf:
+                alpha = 2*alpha
+            else:
+                alpha = (alpha_min + alpha_max)/2
+        else:
+            return alpha
 
 def steepestDescent(f, df, z_list, n, x):
     xk_prev = x
-    A, c = q4.construct_A_and_C(n, x)
-    b = 0
-    # p is the search direction. Different approaches for finding it yields different algorithms
-    gradient = df(z_list, n, A, b, c)
-    p = - gradient/np.linalg.norm(gradient, 2)
-    alpha = backtrackingLinesearch(f, df, z_list, n, p, x) # finding step length
-    xk = xk_prev + alpha * gradient * p
-    while np.linalg.norm(xk-xk_prev, 2) > 10e-4:
-        A, c = q4.construct_A_and_C(n, x)
-        gradient = df(z_list, n, A, b, c)
-        p = - gradient / np.linalg.norm(gradient, 2)
-        alpha = backtrackingLinesearch(f, df, z_list, n, p, x)  # finding step length
-        xk, xk_prev = xk + alpha * gradient * p, xk
+    xk = x
+    p = - df(z_list, n, xk) # descent direction
+    alpha = armijoBacktracking(f, df, z_list, n, p, xk) # step length
+    xk = xk_prev + alpha * p
+    while f(z_list, n, xk) > 0 and np.linalg.norm(df(z_list, n, x), 2) > 10e-4:
+        p = - df(z_list, n, xk)
+        alpha = armijoBacktracking(f, df, z_list, n, p, xk)
+        xk, xk_prev = xk + alpha * p, xk
     return xk
 
-def quasiNewton():
-    p = 0 # something something
+# A Quasi-Newton Method
+def BFGS(f, df, z_list, n, xk):
+    Hk = np.identity(int(n * (n + 1) / 2) + n)
+    while f(z_list, n, xk) > 0 and np.linalg.norm(df(z_list, n, x), 2) > 10e-4:
+        p = - np.matmul(Hk, df(z_list, n, xk))
+        alpha = armijoBacktracking(f, df, z_list, n, p, xk)
+        xk, xk_prev = xk + alpha * p, xk
+        print("\nxk", xk)
+        print("f(xk)", f(z_list, n, xk))
+        sk = xk - xk_prev
+        yk = df(z_list, n, xk) - df(z_list, n, xk_prev)
+        rho = 1 / np.dot(yk, sk)
+        Hk = np.matmul(np.matmul((np.identity(int(n * (n + 1) / 2) + n) - rho * np.dot(sk, yk)), Hk),
+                                (np.identity(int(n * (n + 1) / 2) + n) - rho * np.dot(yk, sk))) + rho * np.dot(sk,
+                                                                                                               sk)
+    return xk
+
+def SR1(f, df, z_list, n, x):
     return 0
 
 if __name__ == "__main__":
-    # Følgende er blåkopi fra q4 26.02
-    # dimensions must be n = int(k*(k+1)/2) such that k is an integer
-    n = 6
-    dim = int(n*(n+1)/2) + n
-    m = 3  # number of z points
+    n = 2
+    dim = int(n * (n + 1) / 2) + n
+    m = 4  # number of z points
     x = np.ones(dim)
     z_list = np.zeros((m, n + 1))
     for i in range(m):
         z_list[i] = np.ones(n + 1) * i
-        if i < int(m / 2):
+        if abs(i-1.5) > 1:
             z_list[i][0] = -1
         else:
             z_list[i][0] = 1
-    print("n", n)
-    print("dim of x", dim)
     # m=3 gir to w = -1 og en w = 1
-    A, c = q4.construct_A_and_C(n, x)
-
-    # optimization algorithms require f and x0. They find direction p locally in their own way
-    print("value of model 1 at x0:", q4.f_model_1(z_list, A, c))
-    steepestDescent(q4.f_model_1, q4.df_model_1, z_list, n, x)
+    print("n", n)
+    print("xo", x)
+    result_x = steepestDescent(q4.f_model_1, q4.df_model_1, z_list, n, x)
+    print("value of model 1 at end of steepest descent:", q4.f_model_1(z_list, n, result_x))
+    result_x = BFGS(q4.f_model_1, q4.df_model_1, z_list, n, x)
+    print("value of model 1 at end of BFGS:", q4.f_model_1(z_list, n, result_x))
